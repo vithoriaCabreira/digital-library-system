331abb017815b659c40b4449d83f17aea514407a
Vithoria Cabreira <vithoriacabreira@alunos.utfpr.edu.br>
2026-08-05 17:53:07 -0300

    melhorando o oódigo

--- include/livro.h
+++ include/livro.h
@@ -1,11 +1,15 @@
 #ifndef LIVRO_H //inclui a struct livro apenas uma vez no codigo
 #define LIVRO_H
 
+#define MAX_TITULO 100 //define o tamanho maximo do titulo do livro
+#define MAX_AUTOR 100 //define o tamanho maximo do autor do livro
+#define MAX_EDITORA 100 //define o tamanho maximo da editora do livro
+
 typedef struct{ //usando o typedef para criar um apelido para a struct
     int id;
-    char titulo[100];
-    char autor[100];
-    char editora[100];
+    char titulo[MAX_TITULO];
+    char autor[MAX_AUTOR];
+    char editora[MAX_EDITORA];
     int anoPublicacao;
     int quntidadeTotal;
     int quantidadeDisponivel;
